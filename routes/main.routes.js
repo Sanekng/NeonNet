fa380482b48da4ef251acae26586ebee55e61388
Sanekng <sanekng@gmail.com>
2024-07-20 18:27:00 +0200

    finished setting up the routes leading to the pages. form logic left. For the future authentication will be added

--- routes/main.routes.js
+++ routes/main.routes.js
@@ -9,4 +9,25 @@ routes.get('/newsets', (req, res)=>{
     res.render('newsets.ejs');
 })
 
-module.exports = routes;
+routes.get('/material-one', (req, res)=>{
+    res.render('material-one.ejs');
+})
+
+routes.get('/cart', (req, res)=>{
+    res.render('cart.ejs')
+});
+
+routes.get('/about-us', (req, res)=>{
+    res.render('about-us.ejs');
+});
+
+routes.get('/contact-us', (req, res)=>{
+    res.render('contact-us.ejs');
+});
+
+routes.post('/contact-us', (req, res)=>{
+    res.redirect('/')
+})
+
+
+module.exports = routes; 
